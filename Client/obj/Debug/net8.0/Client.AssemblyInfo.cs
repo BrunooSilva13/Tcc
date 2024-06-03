940d8eb62172abd4af42614ba99766ad0374c371
BrunooSilva13 <brunomendes.2259@gmail.com>
2024-06-03 11:35:04 -0300

    feat

--- Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0511c5c994d0bb35284cff7819a6cc35c40d138f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb955bcbc28911d5c3ae35970e5c2ca6f05a4f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
